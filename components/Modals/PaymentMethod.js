76bb86a8958489a89b1a8c56c4a9d8ecedf4c744
suman <ronaldosuman97@gmail.com>
2022-05-06 22:26:42 +0530

    checkout modal layout fixed

--- components/Modals/PaymentMethod.js
+++ components/Modals/PaymentMethod.js
@@ -8,16 +8,19 @@ import {
 } from "react-native";
 import { useTheme } from "@react-navigation/native";
 import { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
 
 const Button = () => {
   return (
-    <TouchableOpacity>
-      <Text>Hi</Text>
+    <TouchableOpacity style={styles.btn_container}>
+      <AntDesign name="creditcard" size={24} color="white" />
+      <Text style={styles.text_btn}>CARD</Text>
     </TouchableOpacity>
   );
 };
 
 const deviceWidth = Dimensions.get("window").width;
+const modalSize = deviceWidth - 50;
 export function PaymentMethod() {
   const { colors } = useTheme();
   const [modal, setModal] = useState(false);
@@ -37,22 +40,26 @@ export function PaymentMethod() {
         }}
       >
         <TouchableOpacity
-          // onPress={() => setModal(false)}
+          onPress={() => setModal(!modal)}
           style={styles.centeredView}
           activeOpacity={1}
         >
           <View
             style={[styles.modalView, { backgroundColor: colors.background }]}
           >
-            <Text style={styles.pay_text}>PAYMENT METHOD</Text>
-            <Button />
+            <View style={styles.pay_text_container}>
+              <Text style={styles.pay_text}>PAYMENT METHOD</Text>
+            </View>
+            <View style={styles.btn_view}>
+              <Button />
+            </View>
             <TouchableOpacity
               style={[
                 styles.cancel_container,
                 { backgroundColor: colors.accent },
               ]}
             >
-              <Text>CANCEL</Text>
+              <Text style={{ fontSize: 17 }}>CANCEL</Text>
             </TouchableOpacity>
           </View>
         </TouchableOpacity>
@@ -83,13 +90,51 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#d22",
+    backgroundColor: "rgba(0,0,0,.5)",
   },
   modalView: {
-    width: deviceWidth - 50,
-    height: 300,
+    width: modalSize,
+    height: modalSize * 1.2,
+    borderRadius: modalSize / 15,
+    overflow: "hidden",
+    alignItems: "center",
+  },
+  pay_text_container: {
+    height: modalSize / 4,
+    justifyContent: "center",
+    alignItems: "center",
   },
   pay_text: {
-    fontSize: 17,
+    fontSize: modalSize / 16,
+    width: "100%",
+    textAlign: "center",
+  },
+  btn_view: {
+    backgroundColor: "#d22",
+    width: "100%",
+    height: modalSize * 1.2 - (modalSize / 4 + modalSize / 6.5),
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn_container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+    width: 150,
+    height: 50,
+    borderRadius: 10,
+  },
+  text_btn: {
+    color: "#FFF",
+    marginLeft: 10,
+  },
+  cancel_container: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    height: modalSize / 6.5,
   },
 });
